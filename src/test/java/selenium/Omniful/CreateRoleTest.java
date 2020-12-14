e2c6f22c36a889d4c79b6f00e1469d18f87fab1f
asmaaabdelkerim <asmaaabdelkerim@outlook.com>
2020-12-14 23:25:54 +0200

    first commit

---
+++ src/test/java/selenium/Omniful/CreateRoleTest.java
@@ -0,0 +1,39 @@
+package selenium.Omniful;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class CreateRoleTest extends TestBases {
+
+	@BeforeTest
+	public void Openbrowser() throws InterruptedException {
+		OpenChrome("https://demo3.dashboard-omniful-stage.ibtikar.sa/pages/login");
+		driver.manage().window().maximize();
+		Thread.sleep(10000);
+	}
+	@Test(priority=1)
+	public void LoginCLient() throws InterruptedException {
+		ClientLoginPage clp = new ClientLoginPage(driver);
+		clp.clientLoginMethod("hadeera00.1@gmail.com", "123456");
+		Thread.sleep(10000);
+	}
+	@Test(priority = 2)
+	public void OpenRoles() throws InterruptedException {
+		ClientHomePage chp=new ClientHomePage(driver);
+		chp.OpenRolesPage();
+		Thread.sleep(10000);
+	}
+	@Test(priority=3)
+	public void OpenAddRole() throws InterruptedException {
+		RolePage rp = new RolePage(driver);
+		rp.AddRole();
+		Thread.sleep(10000);
+	}
+	@Test(priority = 4)
+	public void AddRole() throws InterruptedException {
+		CreateRolePage crp = new CreateRolePage(driver);
+		crp.AddRoleMethod("role123");
+		Thread.sleep(10000);
+	}
+
+}
---
+++ src/test/java/selenium/Omniful/DeleteRoleTest.java
@@ -0,0 +1,51 @@
+package selenium.Omniful;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DeleteRoleTest extends TestBases{
+
+	@BeforeTest
+	public void Openbrowser() throws InterruptedException {
+		OpenChrome("https://demo3.dashboard-omniful-stage.ibtikar.sa/pages/login");
+		driver.manage().window().maximize();
+		Thread.sleep(10000);
+		}
+	
+	
+	@Test(priority=1)
+	public void LoginCLient() throws InterruptedException {
+		ClientLoginPage clp = new ClientLoginPage(driver);
+		clp.clientLoginMethod("hadeera00.1@gmail.com", "123456");
+		Thread.sleep(10000);
+	}
+	@Test(priority = 2)
+	public void OpenRoles() throws InterruptedException {
+		ClientHomePage chp=new ClientHomePage(driver);
+		chp.OpenRolesPage();
+		Thread.sleep(10000);
+	}
+	@Test(priority=3)
+	public void OpenAddRole() throws InterruptedException {
+		RolePage rp = new RolePage(driver);
+		rp.AddRole();
+		Thread.sleep(10000);
+	}
+	@Test(priority = 4)
+	public void AddRole() throws InterruptedException {
+		CreateRolePage crp = new CreateRolePage(driver);
+		crp.AddRoleMethod("role1234");
+		Thread.sleep(10000);
+	}
+	@Test(priority = 5)
+	public void Deletebtn() {
+		RolePage rp = new RolePage(driver);
+		rp.DeleteRole1();
+	}
+	
+	@Test(priority = 6)
+	public void DeleteRole() {
+		DeleteRolePage drp = new DeleteRolePage(driver);
+				drp.YesDeleteRoleMethod();
+	}
+}
